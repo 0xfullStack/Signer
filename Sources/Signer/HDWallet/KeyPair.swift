b4eb78f83c2b3871162466f40292f9d0ed4cb458
Lin <linshizai.me@gmail.com>
2022-06-15 16:59:29 +0800

    make it public

--- Sources/Signer/HDWallet/KeyPair.swift
+++ Sources/Signer/HDWallet/KeyPair.swift
@@ -18,7 +18,7 @@ public struct KeyPair {
     public var `public`: [UInt8]
     public var `private`: [UInt8]
     
-    func sign(_ messageHash: [UInt8]) throws -> [UInt8] {
+    public func sign(_ messageHash: [UInt8]) throws -> [UInt8] {
         let ecKey = ECC.Key(priv: self.private)
         let sig = try ecKey.sign(hash: messageHash)
         return sig.r + sig.s + [UInt8(sig.recoveryParam!)]
