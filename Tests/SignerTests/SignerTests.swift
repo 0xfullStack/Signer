2aff4679a6d1fc30a629f9c6943483b11ffb697f
Lin <linshizai.me@gmail.com>
2022-06-17 15:13:34 +0800

    add test

--- Tests/SignerTests/SignerTests.swift
+++ Tests/SignerTests/SignerTests.swift
@@ -6,5 +6,8 @@ final class SignerTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
+        
+        let signer: Signer = HDWallet()
+        signer.deriveKeyPair(path: "", index: "")
     }
 }
